2667f276f51714c9fb4e87f296e5542ce675336c
jimarasim <jaemzware@hotmail.com>
2014-05-09 16:06:12 -0700

    got to work finally... can read all non-corrupt id3 information

--- AnalogArchive.php
+++ AnalogArchive.php
@@ -1,7 +1,12 @@
 <?php
 
-//https://code.google.com/p/php-reader/downloads/list php mp3 reader
-require_once 'Zend/Media/Id3v2.php';
+
+// include getID3() library (can be in a different directory if full path is specified)
+//http://getid3.sourceforge.net/
+require_once('getid3/getid3.php');
+
+//turn on all error reporting
+error_reporting(E_ALL); 
 
 /**
  * Description of codebase
@@ -10,17 +15,21 @@ require_once 'Zend/Media/Id3v2.php';
  */
 class AnalogArchive {
 
+    private static $mediaFolder = 'images';
+    
     public static function CatalogMedia()
     {
-        $mediaFolder = 'media';
+        // Initialize getID3 engine
+        $getID3 = new getID3;
+        
+        
+        //$mediaFolder = 'media';
         //$mediaFolder='/Users/jameskarasim/Documents/STATIC/Music/_OTHER - VINYL'; //requires chmod o+rx down whole path
         //get a list of files in media folder
-        $files = scandir($mediaFolder);
+        $files = scandir(self::$mediaFolder);
         
         //iterate through files
         foreach ($files as $key => $value) {
-            echo("key:".$key."value:".$value."<br />");
-            
             //find the mp3s
             $pos = strrpos($value, ".mp3",-1);
             if ($pos === false) { // note: three equal signs
@@ -29,23 +38,67 @@ class AnalogArchive {
             else 
             {
                 //found an mp3
-                $filePath = $mediaFolder.'/'.$value;
-                echo($filePath."<br />");
-                
-                $id3 = new Zend_Media_Id3v2($filePath);
-                $TALB = $id3->getFramesByIdentifier("TALB"); // for song title; or TALB for album title; ..
-                $TIT2 = $id3->getFramesByIdentifier("TIT2"); 
-                $TPE1 = $id3->getFramesByIdentifier("TPE1"); 
-                
-                
-                echo($TALB[0]->getText()."<br />".$TIT2[0]->getText()."<br />".$TPE1[0]->getText()."<br />");
-                
-               
-                
+                $filePath = self::$mediaFolder.'/'.$value;
+                //self::ZendDisplayId3Data($filePath);
+                self::GetId3DisplayId3Data($getID3,$filePath);
             }
             
-        }
+       }
     }
+    
+    /**
+     * This function determines the version of id3, and continues processing if so
+     * artist album title track_number comment
+     * @param type $getID3
+     * @param type $filePath
+     */
+    private static function GetId3DisplayId3Data($getID3,$filePath)
+    {
+        
+        echo("FILE:".$filePath."<BR />");
+        
+        // Analyze file and store returned data in $ThisFileInfo
+        $ThisFileInfo = $getID3->analyze($filePath);
+
+        /*
+         Optional: copies data from all subarrays of [tags] into [comments] so
+         metadata is all available in one location for all tag formats
+         metainformation is always available under [tags] even if this is not called
+        */
+        getid3_lib::CopyTagsToComments($ThisFileInfo);
+        
+        if(isset($ThisFileInfo['comments_html']))
+        {
+            foreach($ThisFileInfo['comments_html'] as $property => $value)
+            {
+                echo("PROPERTY:".$property." VALUE:".$value[0]."<BR />");
+            }
+        }
+        else 
+        {
+            echo("INVALID ID3 INFO<BR />");
+        }
+        
+        echo("<hr>");
+    }
+    
+    /**
+    * this function gets the current url
+    */
+   public static function GetHostUrl()
+   {
+       $HTTPS = filter_input(INPUT_SERVER, 'HTTPS');
+       $HTTP_HOST = filter_input(INPUT_SERVER, 'HTTP_HOST');
+       $REQUEST_URI = filter_input(INPUT_SERVER, 'REQUEST_URI');
+
+       $protocol = (!empty($HTTPS) && $HTTPS == 'on') ?'htts://':'http://';
+
+       $currentUrl = $protocol.$HTTP_HOST.$REQUEST_URI;
+
+       return $currentUrl;
+   }
+
+
     
     
 }
--- index.php
+++ index.php
@@ -6,6 +6,21 @@ include 'AnalogArchive.php';
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-AnalogArchive::CatalogMedia();
+?>
+<html><head><title>Analog Archive</title></head>
+    <body>
+        <?php
+            echo(AnalogArchive::GetHostUrl()."<br /><hr>");
+            
+            try
+            {
+                AnalogArchive::CatalogMedia();
+            }
+            catch(Exception $ex)
+            {
+                echo("exception:".$ex->getMessage()."<br />trace:".$ex->getTraceAsString());
+            }
+        ?>
+    </body>
+</html>
 
