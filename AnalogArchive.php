382fd8d73660399aa26fbe7c163e51e592401d8f
jimarasim <jaemzware@hotmail.com>
2014-05-14 06:25:02 -0700

    got array serialized and information I want

--- AnalogArchive.php
+++ AnalogArchive.php
@@ -16,6 +16,7 @@ error_reporting(E_ALL);
 class AnalogArchive {
 
     private static $mediaFolder = 'images';
+    private static $emptyVal = '';
     
     public static function CatalogMedia()
     {
@@ -28,34 +29,46 @@ class AnalogArchive {
         //get a list of files in media folder
         $files = scandir(self::$mediaFolder);
         
+        
+        $songs = array();
+        
         //iterate through files
-        foreach ($files as $key => $value) {
+        foreach ($files as $value) {
             //find the mp3s
             $pos = strrpos($value, ".mp3",-1);
-            if ($pos === false) { // note: three equal signs
-                // not found...
-            }
-            else 
+            if ($pos !== false) 
             {
                 //found an mp3
                 $filePath = self::$mediaFolder.'/'.$value;
                 //self::ZendDisplayId3Data($filePath);
-                self::GetId3DisplayId3Data($getID3,$filePath);
+                self::GetId3DisplayId3Data($getID3,$filePath,$songs);
             }
             
        }
+       
+       //print out songs
+       foreach ($songs as $key => $value) {
+           echo("<a href='".$key."' target='_blank'>".$key."</a><br />");
+           echo("ARTIST:".$value["artist"]."<br />");
+           echo("ALBUM:".$value["album"]."<br />");
+           echo("TITLE:".$value["title"]."<br />");
+           echo("TRACK:".$value["track"]."<br />");
+           
+       }
+       
+       //serialize the data on the client
+       echo("<script>var songsarray = ".json_encode($songs).";</script>");
     }
     
     /**
      * This function determines the version of id3, and continues processing if so
      * artist album title track_number comment
-     * @param type $getID3
-     * @param type $filePath
+     * @param type $getID3 - id3 engine
+     * @param type $filePath - path to the file
+     * @param type &$songs - songs array to populate
      */
-    private static function GetId3DisplayId3Data($getID3,$filePath)
+    private static function GetId3DisplayId3Data($getID3,$filePath,&$songs)
     {
-        
-        echo("FILE:".$filePath."<BR />");
         
         // Analyze file and store returned data in $ThisFileInfo
         $ThisFileInfo = $getID3->analyze($filePath);
@@ -67,19 +80,68 @@ class AnalogArchive {
         */
         getid3_lib::CopyTagsToComments($ThisFileInfo);
         
+        //get all tag comments we care about
         if(isset($ThisFileInfo['comments_html']))
         {
-            foreach($ThisFileInfo['comments_html'] as $property => $value)
+            //get artist
+            if(isset($ThisFileInfo['comments_html']['artist']))
             {
-                echo("PROPERTY:".$property." VALUE:".$value[0]."<BR />");
+                $artist=$ThisFileInfo['comments_html']['artist'][0];
             }
+            else
+            {
+                $artist=self::$emptyVal;
+            }
+            
+            //get album
+            if(isset($ThisFileInfo['comments_html']['album']))
+            {
+                $album=$ThisFileInfo['comments_html']['album'][0];
+            }
+            else
+            {
+                $album=self::$emptyVal;
+            }
+            
+            //get title
+            if(isset($ThisFileInfo['comments_html']['title']))
+            {
+                $title=$ThisFileInfo['comments_html']['title'][0];
+            }
+            else
+            {
+                $title=self::$emptyVal;
+            }
+            
+            //get track
+            if(isset($ThisFileInfo['comments_html']['track']))
+            {
+                $track=$ThisFileInfo['comments_html']['track'][0];
+            }
+            else
+            {
+                $track=self::$emptyVal;
+            }
+            
+            //use this to display all tags gotten
+//            foreach($ThisFileInfo['comments_html'] as $property => $value)
+//            {
+//                echo("PROPERTY:".$property." VALUE:".$value[0]."<BR />");
+//            }
         }
         else 
         {
-            echo("INVALID ID3 INFO<BR />");
+            $artist=self::$emptyVal;
+            $album=self::$emptyVal;
+            $title=self::$emptyVal;
+            $track=self::$emptyVal;
+            //echo("INVALID ID3 INFO<BR />");
         }
         
-        echo("<hr>");
+        //add song to array of all songs found
+        $songs[$filePath]=array("artist"=>$artist,"album"=>$album,"title"=>$title,"track"=>$track);
+        
+        
     }
     
     /**
