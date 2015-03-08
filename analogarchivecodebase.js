ba73f11531076b4f46eb39ff0a8a8c0c0ab94342
James Arasim <jaemzware@hotmail.com>
2015-03-08 11:52:21 -0700

    another cosole output

--- analogarchivecodebase.js
+++ analogarchivecodebase.js
@@ -159,6 +159,7 @@ function SetupEvents()
         for (var i=0;i<sorted.length;i++)
         { 
             console.log(sorted[i].artist);
+            console.log(filteredArtist);
             if(sorted[i].artist===filteredArtist || filteredArtist===allArtistsEnum)
             {
                 $("#songsTable").append(GetSongRow(sorted[i].file,sorted[i].artist,sorted[i].album,sorted[i].title,sorted[i].track,sorted[i].modifiedDate));
